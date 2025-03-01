d3e19e06107a9d681671836a1d030c832a0e2c8a
Blake C <blake.campbell09@gmail.com>
2025-03-01 04:56:26 -0800

    Initial Commit
    Started moving old config over to nixvim
    steven-na/nvim-config -> steven-na/nixvim-config

---
+++ config/binds.nix
@@ -0,0 +1,35 @@
+{
+    keymaps = [
+        {
+            mode = [ "n" "i" ];
+            key = "<Insert>";
+            action = "<Nop>";
+            options = {
+                noremap = true;
+                silent = true;
+            };
+        }
+        {
+            mode = [ "n" ];
+            key = "<C-s>";
+            action = ":w<CR>";
+            options = {
+                noremap = true;
+                silent = true;
+            };
+        }
+        {
+            mode = [ "n" ];
+            key = "<Esc>";
+            action = "<cmd>nohlsearch<CR>";
+        }
+        {
+            mode = [ "t" ];
+            key = "<Esc><Esc>";
+            action = "<C-\\><C-n>";
+            options = {
+                desc = "Exit terminal mode";
+            };
+        }
+    ];
+}
---
+++ config/default.nix
@@ -0,0 +1,9 @@
+{
+  imports = [
+    ./plugins/bufferline.nix
+    ./plugins/lualine.nix
+
+    ./settings.nix
+    ./binds.nix
+  ];
+}

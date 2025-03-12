2bacdbfccc2ffd19d140cb9c41bdea3e6f3e8d5b
Blake C <bstevencamp@gmail.com>
2025-03-12 04:10:47 -0700

    Added rustc because it is needed for completion on rust standard library stuff

--- config/plugins/rust.nix
+++ config/plugins/rust.nix
@@ -1,8 +1,16 @@
 {
+    pkgs,
+    ...
+}:
+{
     plugins.rustaceanvim = {
         enable = true;
         settings = {
             tools.hover_actions.auto_focus = true;
         };
     };
+
+    extraPackages = with pkgs; [
+        rustc
+    ];
 }
